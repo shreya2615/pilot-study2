b90704647a3f937477098185e8132222d6c26fe9
shreya2615 <shreya.sharma2615@gmail.com>
2025-08-22 11:58:17 -0400

    Update experiment.js

--- experiment.js
+++ experiment.js
@@ -400,11 +400,15 @@ timeline.push({
   type: jsPsychHtmlKeyboardResponse,
   stimulus: `
     <h2>Thank you for participating!</h2>
-    <p>Your responses have been recorded. Please click on the link below to be redirected to cloud research and then close this window.</p>
-    <p>https://connect.cloudresearch.com/participant/project/F09A69B578/complete</p>
+    <p>Your responses have been recorded. Please click on the link below to be redirected to CloudResearch and then close this window.</p>
+    <p>
+      <a href="https://connect.cloudresearch.com/participant/project/F09A69B578/complete" target="_blank">
+        Click here to return to CloudResearch
+      </a>
+    </p>
   `,
   choices: "NO_KEYS",
-  trial_duration: 5000
+  trial_duration: 15000
 });
 
 jsPsych.run(timeline);
